540d366e18eece4950be73b43250d4092cbd5dd8
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-05-04 21:21:57 -0400

    Rename Minkowski type as Macfarlane type

--- macfarlane.go
+++ macfarlane.go
@@ -6,16 +6,16 @@ import (
 	"strings"
 )
 
-var symbM = [4]string{"", "s", "t", "u"}
+var symbMacfarlane = [4]string{"", "s", "t", "u"}
 
-// A Minkowski represents a Minkowski quaternion (also known as a hyperbolic
+// A Macfarlane represents a Macfarlane quaternion (also known as a hyperbolic
 // quaternion) as an ordered list of four float64 values.
-type Minkowski [4]float64
+type Macfarlane [4]float64
 
-// String returns the string representation of a Minkowski value. If z
-// corresponds to the Minkowski quaternion a + bs + ct + du, then the string
+// String returns the string representation of a Macfarlane value. If z
+// corresponds to the Macfarlane quaternion a + bs + ct + du, then the string
 // is "(a+bs+ct+du)", similar to complex128 values.
-func (z *Minkowski) String() string {
+func (z *Macfarlane) String() string {
 	a := make([]string, 9)
 	a[0] = "("
 	a[1] = fmt.Sprintf("%g", z[0])
@@ -29,7 +29,7 @@ func (z *Minkowski) String() string {
 		default:
 			a[j] = fmt.Sprintf("+%g", z[i])
 		}
-		a[j+1] = symbM[i]
+		a[j+1] = symbMacfarlane[i]
 		i++
 	}
 	a[8] = ")"
@@ -37,7 +37,7 @@ func (z *Minkowski) String() string {
 }
 
 // Equals returns true if y and z are equal.
-func (z *Minkowski) Equals(y *Minkowski) bool {
+func (z *Macfarlane) Equals(y *Macfarlane) bool {
 	for i, v := range y {
 		if notEquals(v, z[i]) {
 			return false
@@ -47,17 +47,17 @@ func (z *Minkowski) Equals(y *Minkowski) bool {
 }
 
 // Copy copies x onto z, and returns z.
-func (z *Minkowski) Copy(x *Minkowski) *Minkowski {
+func (z *Macfarlane) Copy(x *Macfarlane) *Macfarlane {
 	for i, v := range x {
 		z[i] = v
 	}
 	return z
 }
 
-// NewMinkowski returns a pointer to an Minkowski value made from four given
+// NewMacfarlane returns a pointer to an Macfarlane value made from four given
 // float64 values.
-func NewMinkowski(a, b, c, d float64) *Minkowski {
-	z := new(Minkowski)
+func NewMacfarlane(a, b, c, d float64) *Macfarlane {
+	z := new(Macfarlane)
 	z[0] = a
 	z[1] = b
 	z[2] = c
@@ -65,8 +65,8 @@ func NewMinkowski(a, b, c, d float64) *Minkowski {
 	return z
 }
 
-// IsMinkowskiInf returns true if any of the components of z are infinite.
-func (z *Minkowski) IsMinkowskiInf() bool {
+// IsMacfarlaneInf returns true if any of the components of z are infinite.
+func (z *Macfarlane) IsMacfarlaneInf() bool {
 	for _, v := range z {
 		if math.IsInf(v, 0) {
 			return true
@@ -75,14 +75,14 @@ func (z *Minkowski) IsMinkowskiInf() bool {
 	return false
 }
 
-// MinkowskiInf returns a pointer to a Minkowski quaternionic infinity value.
-func MinkowskiInf(a, b, c, d int) *Minkowski {
-	return NewMinkowski(math.Inf(a), math.Inf(b), math.Inf(c), math.Inf(d))
+// MacfarlaneInf returns a pointer to a Macfarlane quaternionic infinity value.
+func MacfarlaneInf(a, b, c, d int) *Macfarlane {
+	return NewMacfarlane(math.Inf(a), math.Inf(b), math.Inf(c), math.Inf(d))
 }
 
-// IsMinkowskiNaN returns true if any component of z is NaN and neither is an
+// IsMacfarlaneNaN returns true if any component of z is NaN and neither is an
 // infinity.
-func (z *Minkowski) IsMinkowskiNaN() bool {
+func (z *Macfarlane) IsMacfarlaneNaN() bool {
 	for _, v := range z {
 		if math.IsInf(v, 0) {
 			return false
@@ -96,14 +96,14 @@ func (z *Minkowski) IsMinkowskiNaN() bool {
 	return false
 }
 
-// MinkowskiNaN returns a pointer to a Minkowski quaternionic NaN value.
-func MinkowskiNaN() *Minkowski {
+// MacfarlaneNaN returns a pointer to a Macfarlane quaternionic NaN value.
+func MacfarlaneNaN() *Macfarlane {
 	nan := math.NaN()
-	return NewMinkowski(nan, nan, nan, nan)
+	return NewMacfarlane(nan, nan, nan, nan)
 }
 
 // Scal sets z equal to y scaled by a, and returns z.
-func (z *Minkowski) Scal(y *Minkowski, a float64) *Minkowski {
+func (z *Macfarlane) Scal(y *Macfarlane, a float64) *Macfarlane {
 	for i, v := range y {
 		z[i] = a * v
 	}
@@ -111,12 +111,12 @@ func (z *Minkowski) Scal(y *Minkowski, a float64) *Minkowski {
 }
 
 // Neg sets z equal to the negative of y, and returns z.
-func (z *Minkowski) Neg(y *Minkowski) *Minkowski {
+func (z *Macfarlane) Neg(y *Macfarlane) *Macfarlane {
 	return z.Scal(y, -1)
 }
 
 // Conj sets z equal to the conjugate of y, and returns z.
-func (z *Minkowski) Conj(y *Minkowski) *Minkowski {
+func (z *Macfarlane) Conj(y *Macfarlane) *Macfarlane {
 	z[0] = y[0]
 	for i, v := range y[1:] {
 		z[i+1] = -v
@@ -125,7 +125,7 @@ func (z *Minkowski) Conj(y *Minkowski) *Minkowski {
 }
 
 // Add sets z equal to the sum of x and y, and returns z.
-func (z *Minkowski) Add(x, y *Minkowski) *Minkowski {
+func (z *Macfarlane) Add(x, y *Macfarlane) *Macfarlane {
 	for i, v := range x {
 		z[i] = v + y[i]
 	}
@@ -133,7 +133,7 @@ func (z *Minkowski) Add(x, y *Minkowski) *Minkowski {
 }
 
 // Sub sets z equal to the difference of x and y, and returns z.
-func (z *Minkowski) Sub(x, y *Minkowski) *Minkowski {
+func (z *Macfarlane) Sub(x, y *Macfarlane) *Macfarlane {
 	for i, v := range x {
 		z[i] = v - y[i]
 	}
@@ -142,15 +142,15 @@ func (z *Minkowski) Sub(x, y *Minkowski) *Minkowski {
 
 // Mul sets z equal to the product of x and y, and returns z.
 //
-// The multiplication rule for the basis elements s := Minkowski{0, 1, 0, 0},
-// t := Minkowski{0, 0, 1, 0}, and u := Minkowski{0, 0, 0, 1} is:
-// 		Mul(s, s) = Mul(t, t) = Mul(u, u) = Minkowski{1, 0, 0, 0}
+// The multiplication rule for the basis elements s := Macfarlane{0, 1, 0, 0},
+// t := Macfarlane{0, 0, 1, 0}, and u := Macfarlane{0, 0, 0, 1} is:
+// 		Mul(s, s) = Mul(t, t) = Mul(u, u) = Macfarlane{1, 0, 0, 0}
 // 		Mul(s, t) = -Mul(t, s) = +u
 // 		Mul(t, u) = -Mul(u, t) = +s
 // 		Mul(u, s) = -Mul(s, u) = +t
-func (z *Minkowski) Mul(x, y *Minkowski) *Minkowski {
-	p := new(Minkowski).Copy(x)
-	q := new(Minkowski).Copy(y)
+func (z *Macfarlane) Mul(x, y *Macfarlane) *Macfarlane {
+	p := new(Macfarlane).Copy(x)
+	q := new(Macfarlane).Copy(y)
 	z[0] = (p[0] * q[0]) + (p[1] * q[1]) + (p[2] * q[2]) + (p[3] * q[3])
 	z[1] = (p[0] * q[1]) + (p[1] * q[0]) + (p[2] * q[3]) - (p[3] * q[2])
 	z[2] = (p[0] * q[2]) - (p[1] * q[3]) + (p[2] * q[0]) + (p[3] * q[1])
@@ -159,66 +159,66 @@ func (z *Minkowski) Mul(x, y *Minkowski) *Minkowski {
 }
 
 // Commutator sets z equal to the commutator of x and y, and returns z.
-func (z *Minkowski) Commutator(x, y *Minkowski) *Minkowski {
-	return z.Sub(new(Minkowski).Mul(x, y), new(Minkowski).Mul(y, x))
+func (z *Macfarlane) Commutator(x, y *Macfarlane) *Macfarlane {
+	return z.Sub(new(Macfarlane).Mul(x, y), new(Macfarlane).Mul(y, x))
 }
 
 // Associator sets z equal to the associator of w, x, and y, and returns z.
-func (z *Minkowski) Associator(w, x, y *Minkowski) *Minkowski {
+func (z *Macfarlane) Associator(w, x, y *Macfarlane) *Macfarlane {
 	return z.Sub(
-		new(Minkowski).Mul(new(Minkowski).Mul(w, x), y),
-		new(Minkowski).Mul(w, new(Minkowski).Mul(x, y)),
+		new(Macfarlane).Mul(new(Macfarlane).Mul(w, x), y),
+		new(Macfarlane).Mul(w, new(Macfarlane).Mul(x, y)),
 	)
 }
 
 // AlternatorL sets z equal to the left alternator of x and y, and returns z.
-func (z *Minkowski) AlternatorL(x, y *Minkowski) *Minkowski {
+func (z *Macfarlane) AlternatorL(x, y *Macfarlane) *Macfarlane {
 	return z.Associator(x, x, y)
 }
 
 // AlternatorR sets z equal to the right alternator of x and y, and returns z.
-func (z *Minkowski) AlternatorR(x, y *Minkowski) *Minkowski {
+func (z *Macfarlane) AlternatorR(x, y *Macfarlane) *Macfarlane {
 	return z.Associator(x, y, y)
 }
 
 // Quad returns the quadrance of z, which can be either positive, negative or
 // zero.
-func (z *Minkowski) Quad() float64 {
-	return (new(Minkowski).Mul(z, new(Minkowski).Conj(z)))[0]
+func (z *Macfarlane) Quad() float64 {
+	return (new(Macfarlane).Mul(z, new(Macfarlane).Conj(z)))[0]
 }
 
 // IsZeroDiv returns true if z is a zero divisor (i.e. it has zero quadrance).
-func (z *Minkowski) IsZeroDiv() bool {
+func (z *Macfarlane) IsZeroDiv() bool {
 	return !notEquals(z.Quad(), 0)
 }
 
 // Inv sets z equal to the inverse of x, and returns z. If x is a zero divisor,
 // then Inv panics.
-func (z *Minkowski) Inv(x *Minkowski) *Minkowski {
+func (z *Macfarlane) Inv(x *Macfarlane) *Macfarlane {
 	if x.IsZeroDiv() {
 		panic("inverse of zero divisor")
 	}
-	return z.Scal(new(Minkowski).Conj(x), 1/x.Quad())
+	return z.Scal(new(Macfarlane).Conj(x), 1/x.Quad())
 }
 
 // Quo sets z equal to the quotient of x and y, and returns z. If y is a zero
 // divisor, then Quo panics.
-func (z *Minkowski) Quo(x, y *Minkowski) *Minkowski {
+func (z *Macfarlane) Quo(x, y *Macfarlane) *Macfarlane {
 	if y.IsZeroDiv() {
 		panic("denominator is zero divisor")
 	}
-	return z.Scal(new(Minkowski).Mul(x, new(Minkowski).Conj(y)), 1/y.Quad())
+	return z.Scal(new(Macfarlane).Mul(x, new(Macfarlane).Conj(y)), 1/y.Quad())
 }
 
 // IsIndempotent returns true if z is an indempotent (i.e. if z = z*z).
-func (z *Minkowski) IsIndempotent() bool {
-	return z.Equals(new(Minkowski).Mul(z, z))
+func (z *Macfarlane) IsIndempotent() bool {
+	return z.Equals(new(Macfarlane).Mul(z, z))
 }
 
-// RectMinkowski returns a Minkowski value made from given curvilinear
+// RectMacfarlane returns a Macfarlane value made from given curvilinear
 // coordinates and quadrance sign.
-func RectMinkowski(r, ξ, θ1, θ2 float64, sign int) *Minkowski {
-	z := new(Minkowski)
+func RectMacfarlane(r, ξ, θ1, θ2 float64, sign int) *Macfarlane {
+	z := new(Macfarlane)
 	if sign > 0 {
 		z[0] = r * math.Cosh(ξ)
 		z[1] = r * math.Sinh(ξ) * math.Cos(θ1)
@@ -240,9 +240,9 @@ func RectMinkowski(r, ξ, θ1, θ2 float64, sign int) *Minkowski {
 	return z
 }
 
-// Curv returns the curvilinear coordinates of a Minkowski value, along with
+// Curv returns the curvilinear coordinates of a Macfarlane value, along with
 // the sign of the quadrance.
-func (z *Minkowski) Curv() (r, ξ, θ1, θ2 float64, sign int) {
+func (z *Macfarlane) Curv() (r, ξ, θ1, θ2 float64, sign int) {
 	quad := z.Quad()
 	h := math.Hypot(z[2], z[3])
 	θ1 = math.Atan2(h, z[1])
---
+++ macfarlane_test.go
@@ -0,0 +1,78 @@
+package qtr
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleMacfarlaneInf() {
+	fmt.Println(MacfarlaneInf(-1, 0, 0, 0))
+	fmt.Println(MacfarlaneInf(0, -1, 0, 0))
+	fmt.Println(MacfarlaneInf(0, 0, -1, 0))
+	fmt.Println(MacfarlaneInf(0, 0, 0, -1))
+	// Output:
+	// (-Inf+Infs+Inft+Infu)
+	// (+Inf-Infs+Inft+Infu)
+	// (+Inf+Infs-Inft+Infu)
+	// (+Inf+Infs+Inft-Infu)
+}
+
+func ExampleMacfarlaneNaN() {
+	fmt.Println(MacfarlaneNaN())
+	// Output:
+	// (NaN+NaNs+NaNt+NaNu)
+}
+
+func ExampleNewMacfarlane() {
+	fmt.Println(NewMacfarlane(1, 0, 0, 0))
+	fmt.Println(NewMacfarlane(0, 1, 0, 0))
+	fmt.Println(NewMacfarlane(0, 0, 1, 0))
+	fmt.Println(NewMacfarlane(0, 0, 0, 1))
+	fmt.Println(NewMacfarlane(1, 2, 3, 4))
+	// Output:
+	// (1+0s+0t+0u)
+	// (0+1s+0t+0u)
+	// (0+0s+1t+0u)
+	// (0+0s+0t+1u)
+	// (1+2s+3t+4u)
+}
+
+func TestMacfarlaneAdd(t *testing.T) {}
+
+func TestMacfarlaneAlternatorL(t *testing.T) {}
+
+func TestMacfarlaneAlternatorR(t *testing.T) {}
+
+func TestMacfarlaneAssociator(t *testing.T) {}
+
+func TestMacfarlaneCommutator(t *testing.T) {}
+
+func TestMacfarlaneConj(t *testing.T) {}
+
+func TestMacfarlaneCopy(t *testing.T) {}
+
+func TestMacfarlaneEquals(t *testing.T) {}
+
+func TestMacfarlaneInv(t *testing.T) {}
+
+func TestIsMacfarlaneInf(t *testing.T) {}
+
+func TestMacfarlaneIsIndempotent(t *testing.T) {}
+
+func TestIsMacfarlaneNaN(t *testing.T) {}
+
+func TestMacfarlaneIsZeroDiv(t *testing.T) {}
+
+func TestMacfarlaneMul(t *testing.T) {}
+
+func TestMacfarlaneNeg(t *testing.T) {}
+
+func TestMacfarlaneQuad(t *testing.T) {}
+
+func TestMacfarlaneQuo(t *testing.T) {}
+
+func TestMacfarlaneScal(t *testing.T) {}
+
+func TestMacfarlaneString(t *testing.T) {}
+
+func TestMacfarlaneSub(t *testing.T) {}
